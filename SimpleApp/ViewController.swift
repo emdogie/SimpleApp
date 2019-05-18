5d732dee7a4eed3250ed955b0499b45a5e026702
marek <marek@macbook-pro-marek.local>
2019-05-19 00:14:32 +0200

    Initial Commit

---
+++ SimpleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleApp
+//
+//  Created by marek on 19.05.2019.
+//  Copyright © 2019 Marek Garczewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
